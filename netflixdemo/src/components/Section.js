829635e5fc932ed1309d1de08af7c5a4c3ee5f6c
RachelC <0722RachelC@users.noreply.github.com>
2020-04-09 11:50:06 -0400

    demo.v3
    1. update Section calss component to function one

--- netflixdemo/src/components/Section.js
+++ netflixdemo/src/components/Section.js
@@ -1,34 +1,23 @@
-import React, { Children } from "react";
+import React from "react";
 import MovieCard from "./MovieCard";
 import "./style.css";
-class Section extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      target: null,
-    };
-    // this.handleClick = this.handleClick.bind(this);
-  }
-
-  render() {
-    const { data, children, buttonInfo, onClick } = this.props;
-    return (
-      <div className="section">
-        <p className="section-name">{children}</p>
-        <ul>
-          {data.map(({ id, img, title }) => (
-            <MovieCard
-              id={id}
-              img={img}
-              title={title}
-              onClick={() => onClick(id)}
-              buttonInfo={buttonInfo}
-            />
-          ))}
-        </ul>
-      </div>
-    );
-  }
+function Section({ data, children, buttonInfo, onClick }) {
+  return (
+    <div className="section">
+      <p className="section-name">{children}</p>
+      <ul>
+        {data.map(({ id, img, title }) => (
+          <MovieCard
+            id={id}
+            img={img}
+            title={title}
+            onClick={() => onClick(id)}
+            buttonInfo={buttonInfo}
+          />
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default Section;
